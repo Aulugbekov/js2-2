fd55eb19178729af87e8416a4fbe707175be314c
Aulugbekov <119114287+Aulugbekov@users.noreply.github.com>
2023-01-24 12:46:20 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,4 @@
 console.log("Client request");
-const person = {
-  name: "USER1",
-  number: "1235413637",
-  age: "23",
-  status: "online",
-  sallary: "3000$",
-  plaseOfBirth: "Kyrgyzstan",
-};
 
 // setTimeout(() => {
 //   console.log("Json Data is ready");
@@ -40,24 +32,38 @@ const person = {
 //   }, 1000);
 // });
 
-const JsonData = JSON.stringify(person);
-const parsedData = JSON.parse(JsonData);
+// const JsonData = JSON.stringify(person);
+// const parsedData = JSON.parse(JsonData);
 // Создаётся объект promise
 let promise = new Promise((resolve, reject) => {
+  const person = {
+    name: "USER1",
+    number: "1235413637",
+    age: "23",
+    status: "online",
+    sallary: "3000$",
+    plaseOfBirth: "Kyrgyzstan",
+  };
   setTimeout(() => {
     console.log("Json Data is ready...");
-    const JsonData = JSON.stringify(person);
-    console.log(person, JsonData);
-    resolve();
+    const jsonData = JSON.stringify(person);
+    console.log(person, jsonData);
+    resolve(jsonData);
   }, 2000);
 });
 
 // promise.then навешивает обработчики на успешный результат или ошибку
-promise.then(
-  setTimeout(() => {
-    console.log("Parsed data is ready...");
-    const JsonParsed = JSON.parse(JsonData);
-    console.log(JsonParsed);
-    console.log("loading end");
-  }, 3000)
-);
+// promise.then((jsonData) => {}
+//   setTimeout(() => {
+promise
+  .then((jsonData) => {
+    setTimeout(() => {
+      console.log("Parsed data is ready...");
+      const JsonParsed = JSON.parse(jsonData);
+      console.log(JsonParsed);
+      console.log("loading end");
+    }, 3000);
+  })
+  .catch(() => {
+    console.log("error");
+  });
