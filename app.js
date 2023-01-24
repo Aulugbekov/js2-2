a5678397e37aee3de9e037badc39569ace975fab
Timur <51020673+Timau32@users.noreply.github.com>
2023-01-24 15:12:15 +0600

    изменил на корректную версию
    перетащил инициализацию внутрь промисаь и передал данные через resolve на след блок then

--- app.js
+++ app.js
@@ -1,12 +1,5 @@
 console.log("Client request");
-const person = {
-  name: "USER1",
-  number: "1235413637",
-  age: "23",
-  status: "online",
-  sallary: "3000$",
-  plaseOfBirth: "Kyrgyzstan",
-};
+
 
 // setTimeout(() => {
 //   console.log("Json Data is ready");
@@ -40,24 +33,33 @@ const person = {
 //   }, 1000);
 // });
 
-const JsonData = JSON.stringify(person);
-const parsedData = JSON.parse(JsonData);
+// const JsonData = JSON.stringify(person);
+// const parsedData = JSON.parse(JsonData);
 // Создаётся объект promise
 let promise = new Promise((resolve, reject) => {
+    const person = {
+    name: "USER1",
+    number: "1235413637",
+    age: "23",
+    status: "online",
+    sallary: "3000$",
+    plaseOfBirth: "Kyrgyzstan",
+  };
+  
   setTimeout(() => {
     console.log("Json Data is ready...");
     const JsonData = JSON.stringify(person);
     console.log(person, JsonData);
-    resolve();
+    resolve(person);
   }, 2000);
 });
 
 // promise.then навешивает обработчики на успешный результат или ошибку
-promise.then(
-  setTimeout(() => {
+promise.then((jsonData) => {
+   setTimeout(() => {
     console.log("Parsed data is ready...");
-    const JsonParsed = JSON.parse(JsonData);
+    const JsonParsed = JSON.parse(jsonData);
     console.log(JsonParsed);
     console.log("loading end");
   }, 3000)
-);
+});
